9de916e396a6469940730f92a7e67d6b5414f454
Liso17 <liso_dm@hotmail.com>
2024-06-11 20:16:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let menuVisible = false;
+function mostrarOcultarMenu (){
+    if(menuVisible){
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("office");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("dedicacion");
+        habilidades[7].classList.add("autodidacta");
+        habilidades[8].classList.add("responsabilidad");
+        habilidades[9].classList.add("puntualidad");
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
